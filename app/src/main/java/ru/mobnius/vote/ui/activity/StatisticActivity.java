832165339a60330982d4731ce14c1046428c8160
Александр Краснов <akrasnov87@gmail.com>
2020-05-21 14:34:38 +0300

    ref statistic

--- app/src/main/java/ru/mobnius/vote/ui/activity/StatisticActivity.java
+++ app/src/main/java/ru/mobnius/vote/ui/activity/StatisticActivity.java
@@ -1,9 +1,14 @@
 package ru.mobnius.vote.ui.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import java.util.Objects;
+
 import ru.mobnius.vote.R;
 import ru.mobnius.vote.data.manager.BaseActivity;
 import ru.mobnius.vote.data.manager.exception.IExceptionCode;
@@ -12,27 +17,37 @@ import ru.mobnius.vote.data.manager.exception.IExceptionCode;
  * Статистика
  */
 public class StatisticActivity extends BaseActivity {
-    private TextView apartmentsTotal;
-    private TextView apartmentsOpen;
-    private TextView signaturesReceived;
+    private TextView tvAppartamentCount;
+    private TextView tvAppartamentOpen;
+    private TextView tvSignatureCount;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_statistic);
+        setContentView(R.layout.activity_statistic);
 
-        apartmentsTotal = findViewById(R.id.fStatistic_tvAppTotal);
-        apartmentsOpen = findViewById(R.id.fStatistic_tvOpenApp);
-        signaturesReceived = findViewById(R.id.fStatistic_tvSignsTotal);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        tvAppartamentCount = findViewById(R.id.statistic_appartament_count);
+        tvAppartamentOpen = findViewById(R.id.statistic_appartament_open);
+        tvSignatureCount = findViewById(R.id.statistic_signature_count);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onStart() {
         super.onStart();
 
-        apartmentsTotal.setText(String.format(getString(R.string.apartment_count), "0"));
-        apartmentsOpen.setText(String.format(getString(R.string.apartment_open), "0"));
-        signaturesReceived.setText(String.format(getString(R.string.signs_received), "0"));
+        tvAppartamentCount.setText(String.format(getString(R.string.apartment_count), "0"));
+        tvAppartamentOpen.setText(String.format(getString(R.string.apartment_open), "0"));
+        tvSignatureCount.setText(String.format(getString(R.string.signs_received), "0"));
     }
 
     @Override
