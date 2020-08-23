6e32849a23758c50176db1838a9ff3bff166ee66
Александр Краснов <akrasnov87@gmail.com>
2020-08-23 16:29:33 +0300

    update

--- app/src/main/java/ru/mobnius/vote/data/service/AutoSyncTimerTask.java
+++ app/src/main/java/ru/mobnius/vote/data/service/AutoSyncTimerTask.java
@@ -1,7 +1,9 @@
 package ru.mobnius.vote.data.service;
 
+import java.util.List;
 import java.util.TimerTask;
 
+import ru.mobnius.vote.data.manager.DataManager;
 import ru.mobnius.vote.data.manager.configuration.PreferencesManager;
 import ru.mobnius.vote.data.manager.synchronization.IProgress;
 import ru.mobnius.vote.data.manager.synchronization.ISynchronization;
@@ -9,11 +11,23 @@ import ru.mobnius.vote.data.manager.synchronization.LiteSynchronization;
 import ru.mobnius.vote.data.manager.synchronization.ServiceSynchronization;
 import ru.mobnius.vote.data.manager.synchronization.utils.transfer.Transfer;
 import ru.mobnius.vote.data.manager.synchronization.utils.transfer.TransferProgress;
+import ru.mobnius.vote.data.storage.models.Results;
 
 class AutoSyncTimerTask extends TimerTask {
+    private boolean isSync() {
+        List<Results> results = DataManager.getInstance().getDaoSession().getResultsDao().loadAll();
+        for (Results result : results) {
+            if (!result.isSynchronization) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     @Override
     public void run() {
-        if(PreferencesManager.getInstance().isAutoSync()) {
+        if(PreferencesManager.getInstance().isAutoSync() && isSync()) {
             LiteSynchronization liteSynchronization = LiteSynchronization.getInstance(PreferencesManager.ZIP_CONTENT);
             liteSynchronization.start(null, new IProgress() {
                 @Override
